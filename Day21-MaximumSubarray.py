7d24e96cabcb74a8c1f40b5b1522e823d8113ecc
HP04Harsh <harshpardhi477@gmail.com>
2026-07-12 12:16:17 +0530

    Day 22

---
+++ Day21-MaximumSubarray.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr, res = 0, float('-inf')
+        for i in nums:
+            curr = max(i, curr+i)
+            res = max(res,curr)
+        return res            
