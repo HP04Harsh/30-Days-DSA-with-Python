b6df0ba0603da0cd8d1b6fe8d4ff14dd7cec1ccc
HP04Harsh <harshpardhi477@gmail.com>
2026-08-04 12:42:03 +0530

    Day 4

---
+++ Practice/Day4-MissingElements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMissingElements(self, nums: list[int]) -> list[int]:
+        num_set = set(nums)
+        min_val = min(nums)
+        max_val = max(nums)
+        
+        # Iterate through the range and collect missing integers
+        return [x for x in range(min_val, max_val + 1) if x not in num_set]
